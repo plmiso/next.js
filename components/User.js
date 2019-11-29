bbe83ee72418f17fec2d5ae6252c95ea312addce
plmiso <miloszsokolowski@aol.com>
2019-11-29 11:09:19 +0100

    'styled

--- components/User.js
+++ components/User.js
@@ -5,6 +5,14 @@ const user = (props) =>{
     return(<div>
         <h1>{props.name}</h1>
         <p>Age: {props.age}</p>
+        <style jsx>
+            {`
+            div {
+                border:1px solid #eee;
+                box-shadow: 0 2p 3px #ccc;
+            }
+            `}
+        </style>
     </div>)
 }
 
