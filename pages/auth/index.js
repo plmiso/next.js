d10881e31fb4d906e3611b24a201be1cccbdc0f4
plmiso <miloszsokolowski@aol.com>
2019-11-29 11:04:45 +0100

    add user component to pages

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,9 +1,11 @@
 import React from "react";
+import User from '../../components/User'
 
 const authIndexPage = () => {
   return (
     <div>
       <h1>Auth Main Page</h1>
+      <User name="Miłosz" age={29} />
     </div>
   );
 };
